c43204a03dceb02f29af9c291b625bffe35d4e6b
tfull <tfurukawa.mail@gmail.com>
2020-04-29 22:38:08 +0900

    pip sample

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name = "wkscraper",
+    version = "0.0.1",
+    description = "Wikipedia Scraper",
+    author = "T.Furukawa"
+)
---
+++ wkscraper/sample.py
@@ -0,0 +1,2 @@
+def sample_function():
+    print("It works")
