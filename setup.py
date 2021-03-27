8362d014fc2cb0022f73b67b7c062fa865289ef8
tfull <tfurukawa.mail@gmail.com>
2021-03-27 15:50:13 +0900

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021 T.Furukawa
 # This software is released under the MIT License, see LICENSE.
 
+import os
 from setuptools import setup, find_packages
 
 
@@ -10,7 +11,7 @@ def get_readme():
 
 
 setup(
-    name = "wikipedia-scraper",
+    name = "wscraper",
     version = "0.0.1",
     license = "MIT License",
     author = "T.Furukawa",
@@ -19,6 +20,8 @@ setup(
     packages = find_packages(),
     keywords = ["Wikipedia"],
     description = "Scraping documents from a dump XML file of Wikipedia.",
+    long_description = get_readme(),
+    long_description_content_type = "text/markdown",
     entry_points = {
         "console_scripts": [
             "wscraper = wscraper.console:command"
